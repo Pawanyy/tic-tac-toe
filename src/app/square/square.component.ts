ed1a5c0d09e74c581e3a69c12f5f2b1793db3c6a
Pawanyy <pawanyyy01@gmail.com>
2023-02-26 13:45:38 +0530

    added grid gaps & color depending on played grid

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,9 +2,12 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  template: ` <button mat-stroked-button>{{ value }}</button> `,
+  template: ` <button *ngIf="value == ''" mat-stroked-button>{{ value }}</button> 
+              <button *ngIf="value == 'X'" color="primary" mat-flat-button>{{ value }}</button>
+              <button *ngIf="value == 'O'" color="accent" mat-flat-button>{{ value }}</button>
+              `,
   styles: ['button { width: 100%; height: 100% !important; font-size: 8rem;}'],
 })
 export class SquareComponent {
-  @Input() value: 'X' | 'O' | ' ' = ' ';
+  @Input() value: 'X' | 'O' | '' = '';
 }
